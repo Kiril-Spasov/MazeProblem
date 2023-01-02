ce22a879c8fb5e3daa8ac808f482e2049445dfee
Kiril Spasov <spasov.kiril@gmail.com>
2023-01-02 12:41:33 +0200

    Add project files.

---
+++ MazeProblem/FormMaze.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MazeProblem
+{
+    partial class FormMaze
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnFindPath = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnFindPath
+            // 
+            this.BtnFindPath.Location = new System.Drawing.Point(49, 55);
+            this.BtnFindPath.Name = "BtnFindPath";
+            this.BtnFindPath.Size = new System.Drawing.Size(183, 65);
+            this.BtnFindPath.TabIndex = 0;
+            this.BtnFindPath.Text = "Find Path";
+            this.BtnFindPath.UseVisualStyleBackColor = true;
+            this.BtnFindPath.Click += new System.EventHandler(this.BtnFindPath_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtResult.Location = new System.Drawing.Point(281, 55);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(446, 346);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(49, 140);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(182, 63);
+            this.BtnExit.TabIndex = 2;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // FormMaze
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnFindPath);
+            this.Name = "FormMaze";
+            this.Text = "Maze";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnFindPath;
+        private System.Windows.Forms.TextBox TxtResult;
+        private System.Windows.Forms.Button BtnExit;
+    }
+}
+
---
+++ MazeProblem/FormMaze.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MazeProblem
+{
+    public partial class FormMaze : Form
+    {
+        public FormMaze()
+        {
+            InitializeComponent();
+        }
+
+        string[,] grid = new string[100, 100];
+        int rows, columns;
+        int rowStart, columnStart;
+        int rowGoal, columnGoal;
+
+        private void BtnFindPath_Click(object sender, EventArgs e)
+        {
+            LoadData(grid);
+            TxtResult.Text += "Original grid: " + Environment.NewLine;
+            Display();
+            TxtResult.Text += Environment.NewLine;
+
+            if (IsPartOfThePath(rowStart, columnStart))
+            {
+                TxtResult.Text += "Exit of the maze is possible, below is the path: " + Environment.NewLine;
+                Display();
+            }
+            else
+            {
+                TxtResult.Text += "There is no possible exit of the maze." + Environment.NewLine;
+            }
+        }
+
+        private void LoadData(string[,] grid)
+        {
+            string path = Application.StartupPath + @"\rmaze.txt";
+
+            StreamReader streamReader = new StreamReader(path);
+
+            string line = "";
+
+            rows = Convert.ToInt32(streamReader.ReadLine());
+            columns = Convert.ToInt32(streamReader.ReadLine());
+
+            for (int i = 1; i <= rows; i++)
+            {
+                line = streamReader.ReadLine();
+
+                for (int j = 1; j <= columns; j++)
+                {
+                    grid[i, j] = line.Substring(j - 1, 1);
+
+                    if (grid[i, j] == "S")
+                    {
+                        rowStart = i;
+                        columnStart = j;
+                    }
+
+                    if (grid[i, j] == "G")
+                    {
+                        rowGoal = i;
+                        columnGoal = j;
+                    }
+                }
+            }
+        }
+
+        private void Display()
+        {
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= columns; j++)
+                {
+                    TxtResult.Text += grid[i, j];
+                }
+                TxtResult.Text += Environment.NewLine;
+            }
+        }
+
+
+        //We ask start position if it's part of the exit path,
+        //then it asks one by one the positions around it if they are part of the exit path,
+        //then each position that is on a valid location knows that it might be a part of the exit path and asks recoursively the positions around it if they are part of the correct path.
+        //If eventually we come to a dead end, all positions return answer false to the position that was on a valid location. Then it continue to ask in the other directions.
+        //If we reach the end position it returns true and we mark it with "p" as part of the exit path,
+        //then all previous positions return answer to the previous that they are actually part of the exit path and we mark them with "p", until we reach back the first position that made the initial call and give the answer to it.
+        private bool IsPartOfThePath(int row, int column)
+        {
+            bool done = false;
+
+            if (CheckLocation(row, column))
+            {
+                //Mark the positions where we have been, so we won't wander around forever.
+                grid[row, column] = "-";
+
+                if (row == rowGoal && column == columnGoal)
+                {
+                    done = true;
+                }
+                else
+                {
+                    done = IsPartOfThePath(row + 1, column);
+
+                    if (!done)
+                        done = IsPartOfThePath(row - 1, column);
+                    if (!done)
+                        done = IsPartOfThePath(row, column + 1);
+                    if (!done)
+                        done = IsPartOfThePath(row, column - 1);
+                }
+            }
+
+            //Mark all positions that return true as part of the exit path.
+            if (done)
+            {
+                grid[row, column] = "p";
+            }
+
+            return done;
+        }
+
+        private bool CheckLocation(int row, int column)
+        {
+            if (row >= 1 && row <= rows && column >= 1 && column <= columns)
+            {
+                if (grid[row, column] == "." || grid[row, column] == "S" || grid[row, column] == "G")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
